a6ec6aeb9b058958c8eef9738221d8bbca87ca7d
Igor Bronowicki <igor.bronowicki@gmail.com>
2012-10-27 16:20:00 +0300

    add TodoList Collection

--- public/index.js
+++ public/index.js
@@ -30,6 +30,59 @@ app.Model.Todo = Backbone.Model.extend({
 });
 
 
+/**
+ * Коллекция {todo} экземпляров.
+ * ----------
+ * Для сохранения используется "localStorage" вместо удаленного сервера.
+ */
+
+app.Collection.TodoList = Backbone.Collection.extend({
+
+    //
+    model: app.Model.Todo,
+
+    //
+    localStorage: new Store('todos-backbone'),
+
+    //
+    completed: function() {
+        return this.filter(function( todo ) {
+            return todo.get('completed');
+        });
+    },
+
+    //
+    remaining: function() {
+      return this.without.apply( this, this.completed() );
+    },
+
+    //
+    nextOrder: function() {
+      if ( !this.length ) {
+        return 1;
+      }
+      return this.last().get('order') + 1;
+    },
+
+    // Один дядька говорил избегать этой хуйни.
+    comparator: function( todo ) {
+      return todo.get('order');
+    }
+
+});
+
+//
+app.Collection.todoList = new app.Collection.TodoList();
+
+
+
+
+
+
+
+
+
+
 $(function(){
     console.log("Hello World!");
 });
