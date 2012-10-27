3ba95791195a5749044811150440547603e31a77
Igor Bronowicki <igor.bronowicki@gmail.com>
2012-10-27 15:59:03 +0300

    add Todo Model

--- public/index.js
+++ public/index.js
@@ -1,3 +1,35 @@
+var app = {
+    Model: {},
+    Collection: {},
+    View: {},
+    Router: {}
+};
+
+
+/**
+ * Модель данных для одного {todo}
+ * ----------
+ * Список атрибутов: "title", "order", и "completed".
+ */
+
+app.Model.Todo = Backbone.Model.extend({
+
+    // По умолчанию каждый созданный экземпляр модели будет иметь атрибуты: "title", и "completed".
+    defaults: {
+        title: '',
+        completed: false
+    },
+
+    // Функция смены логического состояния "completed".
+    toggle: function() {
+        this.save({
+            completed: !this.get('completed')
+        });
+    }
+
+});
+
+
 $(function(){
     console.log("Hello World!");
 });
